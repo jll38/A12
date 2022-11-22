163f4a1ec439a578fcf3b2cc42600f92c5a610cd
jll38 <jll38@njit.edu>
2022-11-22 14:46:36 -0500

    input cookie

--- index.js
+++
@@ -1,7 +0,0 @@
-document.cookie = "username=John Doe";
-const para = document.createElement("p");
-const node = document.createTextNode(document.cookie);
-para.appendChild(node);
-
-const element = document.getElementById(cookieDisplay);
-element.appendChild(para);
